7a453288807cd700f3a11c92229e6ecd4b7cebf7
Adriano de Castro <adrianoautomatic2013@gmail.com>
2024-06-28 08:56:07 -0300

    primeiro comite

---
+++ js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initTooltip(){
+  const tooltips = document.querySelectorAll('[data-tooltip]');
+
+  function criarTooltipBox(element){
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+
+  function onMouseOver(event){
+    const tooltipBox = criarTooltipBox(this);
+    
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+    
+  }
+
+  tooltips.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver);
+  });
+
+  const onMouseMove = {
+    handleEvent(event){
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    },
+  };
+
+  const onMouseLeave = {
+    handleEvent(){
+      this.tooltipBox.remove();
+      this.removeEventListener('mouseleave', onMouseLeave);
+      this.removeEventListener('mousemove', onMouseMove);
+    },  
+  };   
+}
