ae06d3178b46c2f006dbb8c399100e23401e5963
Adriano de Castro <adrianoautomatic2013@gmail.com>
2024-06-28 09:54:56 -0300

    ScrollSuave transformado em Classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,23 +1,34 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+
+  constructor(links, options){
+    this.linksInternos = document.querySelectorAll(links);
+    if(options === undefined){
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+        this.options = options;
+      }
+    this.scrollToSection = this.scrollToSection.bind(this);  
+  }  
   
-  function scrollToSection(event){
+  scrollToSection(event){
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });   
+    section.scrollIntoView(this.options);   
   }
 
-  if(linksInternos.length){
-
-    linksInternos.forEach((link)=> {
-      link.addEventListener('click', scrollToSection);
+  addLinkEvent(){
+    this.linksInternos.forEach((link)=> {
+      link.addEventListener('click', this.scrollToSection);
     });
   }
-}
 
+  init(){
+    if(this.linksInternos.length){
+      this.addLinkEvent();
+    }    
+    return this;
+  }
+}
 
