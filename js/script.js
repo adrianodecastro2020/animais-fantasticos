6aa6216eca259e7c066b45324b356f1c1a8f8b40
Adriano de Castro <adrianoautomatic2013@gmail.com>
2024-07-01 08:48:44 -0300

    tabnav transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
+import TabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -17,9 +17,10 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
 
-initAnimacaoScroll();
-initTabNav();
+
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -27,4 +28,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
-
+initAnimacaoScroll();
