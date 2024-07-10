339d70ea56c4281f2601c02f17da98cbf3fbd878
Adriano de Castro <adrianoautomatic2013@gmail.com>
2024-07-10 10:23:49 -0300

    alteração distancia topo

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,7 +5,7 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = debounce(this.checkDistance.bind(this), 300);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 100);
   }
 
   // Pega a distância de cada item em relação ao topo do site
